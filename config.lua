1f7a9f6d2546326f74c928bea33be431b93e5a11
Ashwin Sundar <ashiundar@gmail.com>
2024-11-07 16:09:20 -0700

    replaces old codemap with a new one

--- config.lua
+++ config.lua
@@ -49,32 +49,47 @@ lvim.plugins = {
       -- log_level = 'debug',
     }
   },
-  {
-      "Isrothy/neominimap.nvim",
-      version = "v3.*.*",
-      enabled = true,
-      lazy = false, -- NOTE: NO NEED to Lazy load
-      -- Optional
-      init = function()
-          -- The following options are recommended when layout == "float"
-          vim.opt.wrap = false
-          vim.opt.sidescrolloff = 36 -- Set a large value
+  -- {
+  --     "Isrothy/neominimap.nvim",
+  --     version = "v3.*.*",
+  --     enabled = true,
+  --     lazy = false, -- NOTE: NO NEED to Lazy load
+  --     -- Optional
+  --     init = function()
+  --         -- The following options are recommended when layout == "float"
+  --         vim.opt.wrap = false
+  --         vim.opt.sidescrolloff = 36 -- Set a large value
 
-          --- Put your configuration here
-          ---@type Neominimap.UserConfig
-          vim.g.neominimap = {
-              auto_enable = true,
-              click = {
-                enabled = true,
-              },
-              float = {
-                minimap_width = 15,
-                window_border = "none", -- "none", "rounded", "solid", "shadow", "single", "double"
-              }
-          }
-      end,
+  --         --- Put your configuration here
+  --         ---@type Neominimap.UserConfig
+  --         vim.g.neominimap = {
+  --             auto_enable = true,
+  --             -- click = {
+  --             --   enabled = true,
+  --             -- },
+  --             layout = "split",
+  --             split = {
+  --               minimap_width = 15,
+  --               window_border = "none", -- "none", "rounded", "solid", "shadow", "single", "double"
+  --             }
+  --         }
+  --     end,
+  -- },
+  {
+    'gorbit99/codewindow.nvim',
+    config = function()
+      local codewindow = require('codewindow')
+      vim.keymap.set('n', '<leader>mm', codewindow.toggle_minimap, { desc = 'toggle minimap' })
+      require('codewindow').setup {
+        auto_enable = true,
+        max_minimap_height = 25,
+        minimap_width = 16,
+        with_multiplier = 10,
+      }
+    end
   },
-{
-  'Exafunction/codeium.vim',
-  event = 'BufEnter'
-}}
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter'
+  }
+}
