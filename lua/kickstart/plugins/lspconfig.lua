1e9f0756edc6935a05e7279c9e0e274072677d38
Ashwin Sundar <ashiundar@gmail.com>
2025-02-14 12:31:25 -0700

    adds jedi python lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -189,7 +189,7 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        jedi_language_server = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -222,6 +222,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'jedi-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
